8d21a8211d106d262c5f2e6a2a08781dc7d8fd2a
ayaal <my@mail.ru>
2022-12-15 00:19:49 +0900

    SF D6 project NEWS PORTAL

---
+++ news_portal/newsportal/views.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = '-date'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
+
